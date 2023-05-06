fed137a0fb13e547e0eeb4c21a5b37766570773d
LemonMints42BKK <131885340+LemonMints42BKK@users.noreply.github.com>
2023-05-06 12:16:30 +0000

    compare fgets version

--- README.md
+++ README.md
@@ -4,8 +4,10 @@
 <div>
 <ul>
 	<ls>andro_fgets():</ls>
-		<p>just like fgets function in the C library function char *fgets(char *str, int n, FILE *stream) reads a line from the specified stream and stores it into the string pointed to by str. It stops when either (n-1) characters are read, the newline character is read, or the end-of-file is reached, whichever comes first. For example, "ls -la\n\0", "cat test.c\0" etc.
-		acepted function: readline()</p>
+		<p>just like fgets function in the C library function
+		<center>char *fgets(char *str, int n, FILE *stream)</center>
+		When fgets is called, it reads characters from the file stream stream and stores them in the buffer pointed to by str. The function reads characters until it encounters a newline character ('\n') or the end of the file, or until n-1 characters have been read, whichever comes first. The newline character, if any, is also stored in the buffer.</p>
+		<p>functions: readline()</p>
 	<ls>History file:</ls>
 <\ul>
 </div>
--- andro_fget.c
+++ andro_fget.c
@@ -6,16 +6,96 @@
 /*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/06 05:27:18 by codespace         #+#    #+#             */
-/*   Updated: 2023/05/06 05:52:25 by codespace        ###   ########.fr       */
+/*   Updated: 2023/05/06 12:10:41 by codespace        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-char	*andro_fget(char *uinput, int n, )
+#include <stdio.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+
+// size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
+
+// char	*andro_fgets(char *buffer, int size, FILE *stream)
+// {
+// 	char	*prompt;
+
+// 	prompt = readline ("Andromeda$ ");
+// 	if (prompt != NULL)
+// 	{
+// 		ft_strlcpy(buffer, prompt, size);
+// 		add_history(prompt);
+// 	}
+// 	free(prompt);
+// 	if (stream != NULL)
+// 	{
+// 		ft_strlcpy(buffer, stream, size);
+// 		add_history(stream);
+// 	}
+
+
+// 	return (buffer);
+// }
+
+// size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
+// {
+// 	size_t	src_len;
+// 	size_t	i;
+
+// 	src_len = ft_strlen(src);
+// 	i = 0;
+// 	if (dstsize)
+// 	{
+// 		while ((i < (dstsize - 1)) && (src[i] != '\0'))
+// 		{
+// 			dst[i] = src[i];
+// 			i++;
+// 		}
+// 		dst[i] = '\0';
+// 	}
+// 	return (src_len);
+// }
+
+char *myfgets(char *str, int n, FILE *stream) {
+    int c;
+    char *p = str;
+
+    /* Make sure we have a valid FILE pointer */
+    if (stream == NULL) {
+        return NULL;
+    }
+
+    /* Read up to n-1 characters from the stream */
+    while (--n > 0 && (c = getc(stream)) != EOF) {
+        *p++ = (char) c;
+
+        /* If we encounter a newline character, stop reading */
+        if (c == '\n') {
+            break;
+        }
+    }
+
+    /* Terminate the string with a null character */
+    *p = '\0';
+
+    /* If no characters were read, return NULL */
+    if (p == str) {
+        return NULL;
+    }
+
+    /* Otherwise, return the string */
+    return str;
+}
+
+int	main(void)
 {
-	char *prompt;
-
-	prompt = (char*)malloc(sizeof(char) * (n + 1));
-	
-
-	return (prompt);
+	char flname[100];
+	char *name;
+	while(1)
+	{
+		printf("Enter your name: ");
+		name = myfgets(flname, sizeof(flname), stdin);
+		printf("Hello %s", name);
+	}
+	return (0);
 }
