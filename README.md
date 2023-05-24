692a9fdb967bf32eef5cee28758fa52433c3deec
LemonMints42BKK <131885340+LemonMints42BKK@users.noreply.github.com>
2023-05-24 04:06:38 +0000

    update

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # andromeda
-miniShell project 
+miniShell project
+
+readline()
+
+char *readline (char *prompt);
+ex:
+	char *line = readline ("Enter a line: ");
+
+The function readline () prints a prompt and then reads and returns a single line of text from the user. The line readline returns is allocated with malloc (); you should free () the line when you are done with it. The declaration for readline in ANSI C is
--- minishell.h
+++ minishell.h
@@ -1,29 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   andromeda.c                                        :+:      :+:    :+:   */
+/*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/05/05 17:09:02 by codespace         #+#    #+#             */
-/*   Updated: 2023/05/05 20:56:29 by codespace        ###   ########.fr       */
+/*   Created: 2023/05/05 17:11:05 by codespace         #+#    #+#             */
+/*   Updated: 2023/05/05 20:23:13 by codespace        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "minishell.h"
-#include <stdio.h>
-#include <readline/readline.h>
-#include <readline/history.h>
+#ifndef MINISHELL_H
+# define MINISHELL_H
 
-int	main(void)
-{
-	static char	*prompt;
+# include <unistd.h>
+# include <stdlib.h>
 
-	prompt = (char*)NULL;
-	while(1 > 0)
-	{
-		prompt = readline ("Andromeda$");
-		free(prompt);
-	}
-	return (0);
-}
+#endif
