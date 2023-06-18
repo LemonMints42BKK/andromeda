04073602930d0258370308ff2d01c31a222f71ae
LemonMints42BKK <131885340+LemonMints42BKK@users.noreply.github.com>
2023-06-18 05:11:08 +0000

    add env_var in t_envlist *envlist : finish created env_var

--- codetest/get_var/main.c
+++ codetest/get_var/main.c
@@ -1,50 +1,103 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <sys/stat.h>
-#include <fcntl.h>
+#include <string.h>
 
-void	ft_putstr_fd(char *s, int fd)
+#define EVN_VARLIST "TERM_PROGRAM TERM SHELL TMPDIR TERM_PROGRAM_VERSION \
+TERM_SESSION_ID USER COMMAND_MODE SSH_AUTH_SOCK __CF_USER_TEXT_ENCODING PATH \
+LaunchInstanceID PWD LANG ITERM_PROFILE XPC_FLAGS XPC_SERVICE_NAME SHLVL HOME \
+COLORFGBG LC_TERMINAL_VERSION LOGNAME ITERM_SESSION_ID LOGNAME LC_TERMINAL \
+SECURITYSESSIONID SQLITE_EXEMPT_PATH_FROM_VNODE_GUARDS COLORTERM"
+
+static size_t	next_pos(char const *s, char c)
 {
-	if (!s)
-		return ;
-	while (*s)
-		write(fd, s++, 1);
+	size_t	index;
+
+	index = 0;
+	while (s[index] != '\0' && s[index] != c)
+		index++;
+	return (index);
 }
 
-// int main() {
-//     int fd;
-//     char* envDir = getenv("HOME");
+static size_t	size_sp(char const *s, char c)
+{
+	char	*ch;
+	size_t	size;
 
-//     fd = open(".env", O_RDWR | O_CREAT | O_APPEND, S_IRUSR | S_IWUSR);
-//     if (fd == -1)
-//     {
-//         printf("Error : history file\n");
-//         return (0);
-//     }
-//     printf("envDir: %s\n", envDir);
-//     if (envDir != NULL) {
-//        ft_putstr_fd(envDir, fd);
-//     } else {
-//         printf("HOME environment variable not found\n");
-//     }
-//     if (close(fd) == -1)
-//     {
-//          printf("Error : close history file\n");
-//         return (0);
-//     }
-//     return 0;
-// }
+	ch = (char *)s;
+	size = 0;
+	while (*ch != '\0')
+	{
+		while (*ch != '\0' && *ch != c)
+			ch++;
+		while (*ch != '\0' && *ch == c)
+			ch++;
+		size++;
+	}
+	return (size);
+}
 
-#include <stdio.h>
-#include <stdlib.h>
+static void	ch_split(char **array, const char *s, char c)
+{
+	char	*ch;
+	size_t	index;
+	size_t	j;
+	size_t	k;
 
-int main() {
-    char* homeDir = getenv("ENV");
-    if (homeDir != NULL) {
-        printf("Home directory: %s\n", homeDir);
-    } else {
-        printf("HOME environment variable not found\n");
-    }
-    return 0;
+	ch = (char *)s;
+	index = next_pos(ch, c);
+	k = 0;
+	while (index)
+	{
+		array[k] = malloc(sizeof(char) * (index + 1));
+		j = 0;
+		while (j < index)
+			array[k][j++] = *(ch++);
+		array[k][j] = '\0';
+		k++;
+		while (*ch != '\0' && *ch == c)
+			ch++;
+		index = next_pos(ch, c);
+	}
+	array[k] = NULL;
+}
+
+char	**ft_split(char const *s, char c)
+{
+	char	**array;
+
+	if (!s)
+		return (NULL);
+	while (*s != '\0' && *s == c)
+		s++;
+	array = malloc(sizeof(char *) * (size_sp(s, c) + 1));
+	if (!array)
+		return (NULL);
+	ch_split(array, s, c);
+	if (!array)
+		free(array);
+	return (array);
+}
+
+int	get_env(void)
+{
+	char	**env_name_list;
+	char	*env_name;
+	char	*env_value;
+
+	env_name_list = ft_split(EVN_VARLIST, ' ');
+	while (*env_name_list)
+	{
+		env_name = *env_name_list;
+		env_value = getenv(env_name);
+		printf("%s=%s\n", env_name, env_value);
+        // printf("%s\n", *env_name_list);
+		env_name_list++;
+	}
+	return (EXIT_SUCCESS);
+}
+
+int main(void)
+{
+	get_env();
+	return (EXIT_SUCCESS);
 }
