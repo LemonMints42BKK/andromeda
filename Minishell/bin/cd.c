1dc21292b66dbbcd8332d38681a6c4e630b47c43
kanitxxx <97750924+kanitxxx@users.noreply.github.com>
2023-06-22 02:57:00 +0000

    cd.c

--- Minishell/bin/cd.c
+++ Minishell/bin/cd.c
@@ -6,12 +6,22 @@
 /*   By: kchatvet <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/21 07:19:11 by kchatvet          #+#    #+#             */
-/*   Updated: 2023/06/21 07:56:18 by kchatvet         ###   ########.fr       */
+/*   Updated: 2023/06/22 02:56:10 by kchatvet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
+int change_directory(const char *path) {
+    if (chdir(path) != 0) {
+        perror("cd");
+        return 1;
+    }
+    
+    return 0;
+}
+
+/*
 void cmd_cd(t_andro shell_var)
 {
     char *home_dir;
@@ -22,3 +32,4 @@ void cmd_cd(t_andro shell_var)
         chdir(home_dir);
     }
 }
+*/
